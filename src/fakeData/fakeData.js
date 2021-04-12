727e6d7982909fb21af62ac2877433655f42aca2
Sakib12345 <sakibalislam12345@gmail.com>
2021-04-12 23:46:12 +0600

    product added

--- src/fakeData/fakeData.js
+++ src/fakeData/fakeData.js
@@ -6,50 +6,50 @@ const fakeData = [{
 },
 {
     title: "Mousum Bay Leaves",
-    quantity: "1kg",
-    price: "234",
+    quantity: "200gm",
+    price: "134",
     image: "https://i.ibb.co/L6XsfPY/image-33.png"
 },
 {
-    title: "Mousum Bay Leaves",
+    title: "Onion Local",
     quantity: "1kg",
-    price: "234",
+    price: "434",
     image: "https://i.ibb.co/cLw25SL/image-34.png"
 },
 {
-    title: "Mousum Bay Leaves",
-    quantity: "1kg",
-    price: "234",
+    title: "Rashid Minicate Rice",
+    quantity: "50kg",
+    price: "334",
     image: "https://i.ibb.co/cQMNsRz/image-35.png"
 },
 {
-    title: "Mousum Bay Leaves",
-    quantity: "1kg",
-    price: "234",
+    title: "Omera LPG Refill",
+    quantity: "12kg",
+    price: "342",
     image: "https://i.ibb.co/PrgDRJg/image-36.png"
 },
 {
-    title: "Mousum Bay Leaves",
-    quantity: "1kg",
-    price: "234",
+    title: "Bombay Sweets Mr.Twist",
+    quantity: "25gm",
+    price: "123",
     image: "https://i.ibb.co/PzSshvM/image-37.png"
 },
 {
-    title: "Mousum Bay Leaves",
-    quantity: "1kg",
-    price: "234",
+    title: "Lifeboy Soap Bar Tota",
+    quantity: "100gm",
+    price: "334",
     image: "https://i.ibb.co/fFJGy5Q/image-38.png"
 },
 {
-    title: "Mousum Bay Leaves",
-    quantity: "1kg",
-    price: "234",
+    title: "Ruchi BBQ Chanachur",
+    quantity: "350gm",
+    price: "342",
     image: "https://i.ibb.co/JyzBVM3/image-39.png"
 },
 {
-    title: "Mousum Bay Leaves",
-    quantity: "1kg",
-    price: "234",
+    title: "Parachute Coconut Oil",
+    quantity: "200ml",
+    price: "213",
     image: "https://i.ibb.co/FXzYZJ0/image-40.png"
 },
 ]
